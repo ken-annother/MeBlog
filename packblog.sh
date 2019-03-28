bea92c193a0f4e6b2b27f444d662d1f86aa06eb7
songkun <jackskin@126.com>
2019-03-28 10:30:31 +0800

    upload build script
    Signed-off-by: songkun <jackskin@126.com>

---
+++ packblog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -rf ../buildtemp
+cp -r . ../buildtemp
+cd ../buildtemp
+sed -i s/"'hostname'        => '127.0.0.1'"/"'hostname'        => 'b-okapcbi3tbctrc.bch.rds.bj.baidubce.com'"/ application/database.php
+sed -i s/"'database'        => 'nk_blog'"/"'database'        => 'b_okapcbi3tbctrc'"/ application/database.php
+sed -i s/"'username'        => 'root'"/"'username'        => 'b_okapcbi3tbctrc'"/ application/database.php
+sed -i s/"'password'        => 'root'"/"'password'        => '7HwzA8LpI9fGBRqb'"/ application/database.php
+sed -i s/"'app_debug'              => true"/"'app_debug'              => false"/ application/config.php
+sed -i s/"'app_trace'              => true"/"'app_trace'              => false"/ application/config.php
+tar -zcf ../nkblog/nkblog.tar.gz .
