c1ee937ca86a3cfa27068bdcca0e619924089d8c
songkun <jackskin@126.com>
2019-12-28 16:00:37 +0800

    modify
    Signed-off-by: songkun <jackskin@126.com>

--- packblog.sh
+++ packblog.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sed -i s/"'hostname'        => '127.0.0.1'"/"'hostname'        => 'b-okapcbi3tbctrc.bch.rds.bj.baidubce.com'"/ application/database.php
-sed -i s/"'database'        => 'nk_blog'"/"'database'        => 'b_okapcbi3tbctrc'"/ application/database.php
-sed -i s/"'username'        => 'root'"/"'username'        => 'b_okapcbi3tbctrc'"/ application/database.php
-sed -i s/"'password'        => 'root'"/"'password'        => '7HwzA8LpI9fGBRqb'"/ application/database.php
-sed -i s/"'app_debug'              => true"/"'app_debug'              => false"/ application/config.php
-sed -i s/"'app_trace'              => true"/"'app_trace'              => false"/ application/config.php
+sed -i s/"'hostname'        => '127.0.0.1'"/"'hostname'        => '$1.bch.rds.bj.baidubce.com'"/ application/database.php
+sed -i s/"'database'        => 'nk_blog'"/"'database'        => '$1'"/ application/database.php
+sed -i s/"'username'        => 'root'"/"'username'        => '$1'"/ application/database.php
+sed -i s/"'password'        => 'root'"/"'password'        => '$2'"/ application/database.php
+sed -i s/"'app_debug' => true"/"'app_debug' => false"/ application/config.php
+sed -i s/"'app_trace' => true"/"'app_trace' => false"/ application/config.php
 
-tar -zcf nkblog.tar.gz .
+tar -zcf ../nkblog.tar.gz .
