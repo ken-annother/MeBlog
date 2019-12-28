f82352cb9c673ab55a74c2506740575db205de86
songkun <jackskin@126.com>
2019-12-28 17:06:16 +0800

    update ci account
    Signed-off-by: songkun <jackskin@126.com>

--- packblog.sh
+++ packblog.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sed -i s/"'hostname'        => '127.0.0.1'"/"'hostname'        => '$1.bch.rds.bj.baidubce.com'"/ application/database.php
-sed -i s/"'database'        => 'nk_blog'"/"'database'        => '$1'"/ application/database.php
-sed -i s/"'username'        => 'root'"/"'username'        => '$1'"/ application/database.php
-sed -i s/"'password'        => 'root'"/"'password'        => '$2'"/ application/database.php
+sed -i s/"'database'        => 'nk_blog'"/"'database'        => '$2'"/ application/database.php
+sed -i s/"'username'        => 'root'"/"'username'        => '$2'"/ application/database.php
+sed -i s/"'password'        => 'root'"/"'password'        => '$3'"/ application/database.php
 sed -i s/"'app_debug' => true"/"'app_debug' => false"/ application/config.php
 sed -i s/"'app_trace' => true"/"'app_trace' => false"/ application/config.php
 
